71a4c68802b658edaf0860300fc6ea56168ff541
user1 <user1@nomail.ru>
2022-04-23 11:11:42 +0300

    deleted main

--- src/ru/zorro/test_git_repo/main/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.zorro.test_git_repo.main;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("--- start ---");
-        System.out.println("--- end ---");
-    }
-}
